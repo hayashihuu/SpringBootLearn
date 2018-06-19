a7f1c186e7cd1fbfe21dcbf8c183e6aac11cb7eb
zhoujun1 <hayashifuu@qq.com>
2018-06-19 14:14:18 +0800

    异步方法

---
+++ springboottest11/src/main/java/com/syun/springboottest11/Springboottest11Application.java
@@ -0,0 +1,27 @@
+package com.syun.springboottest11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+@SpringBootApplication
+@EnableAsync
+public class Springboottest11Application extends AsyncConfigurerSupport {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboottest11Application.class, args);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
+    }
+}
