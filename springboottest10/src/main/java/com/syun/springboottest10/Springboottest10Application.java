e1dab8eca6b64e6544cbc54f13a0f113f32dc6ff
zhoujun1 <hayashifuu@qq.com>
2018-06-19 13:48:17 +0800

    jms使用 未完成,参考使用 主要原因:单元测试的依赖

---
+++ springboottest10/src/main/java/com/syun/springboottest10/Springboottest10Application.java
@@ -0,0 +1,12 @@
+package com.syun.springboottest10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboottest10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboottest10Application.class, args);
+    }
+}
