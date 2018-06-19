9ed079f3a1a83163a12610b66cb8c334f131c814
zhoujun1 <hayashifuu@qq.com>
2018-06-19 09:15:59 +0800

    [200~用restTemplate消费服务

---
+++ springboottest06/src/test/java/com/syunq/springboottest06/Springboottest06ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syunq.springboottest06;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboottest06ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
