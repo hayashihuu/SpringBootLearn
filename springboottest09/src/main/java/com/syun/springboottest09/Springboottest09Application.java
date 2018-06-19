ab3b1673cb66ef8c7aad52522601c05e008ba195
zhoujun1 <hayashifuu@qq.com>
2018-06-19 13:20:49 +0800

    表单验证

---
+++ springboottest09/src/main/java/com/syun/springboottest09/Springboottest09Application.java
@@ -0,0 +1,12 @@
+package com.syun.springboottest09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboottest09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboottest09Application.class, args);
+    }
+}
