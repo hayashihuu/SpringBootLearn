3adee95443b0b9d8ad9b3f183a1c605cbd92adea
zhoujun1 <hayashihuu@yahoo.co.jp>
2018-08-01 13:07:44 +0800

    solr cloud 配置文件上传服务器,文件上传解压及各种操作

---
+++ springboottest16/src/main/java/com/syun/springboottest15/dto/User.java
@@ -0,0 +1,65 @@
+package com.syun.springboottest15.dto;
+
+
+
+import java.util.List;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private List<Role> roles; //测试一对多连接
+
+
+    private Role role;  //测试一对一连接
+
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                ", role=" + role +
+                '}';
+    }
+}
